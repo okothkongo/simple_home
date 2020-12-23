df39b31b53e52b5538d86dd7b214f2cf85bbe64a
okothkongo <okothkongo@gmail.com>
2020-12-23 15:05:37 +0300

    user login

--- test/simple_home_web/integrations/index_page_integration_test.exs
+++ test/simple_home_web/integrations/index_page_integration_test.exs
@@ -1,7 +1,7 @@
 defmodule SimpleHomeWeb.Integrations.IndexPageTest do
   use SimpleHomeWeb.IntegrationCase, async: true
 
-  test "Basic page flow", %{conn: conn} do
+  test "home page  user not logged in", %{conn: conn} do
     get(conn, Routes.page_path(conn, :index))
     |> assert_response(status: 200, path: Routes.page_path(conn, :index))
   end
