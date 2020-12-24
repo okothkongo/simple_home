ae1b4aa20d860037d6a1a1af20da8574aa454e77
okoth kongo <34419890+okothkongo@users.noreply.github.com>
2020-12-24 17:23:43 +0300

    setup navbar (#45)

--- lib/simple_home_web/views/layout_view.ex
+++ lib/simple_home_web/views/layout_view.ex
@@ -1,12 +1,3 @@
 defmodule SimpleHomeWeb.LayoutView do
   use SimpleHomeWeb, :view
-
-  def current_user(conn) do
-    conn.assigns.current_user
-  end
-
-  def first_name(conn) do
-    current_user = current_user(conn)
-    current_user.first_name
-  end
 end
---
+++ lib/simple_home_web/views/shared_view.ex
@@ -0,0 +1,12 @@
+defmodule SimpleHomeWeb.SharedView do
+  use SimpleHomeWeb, :view
+
+  def current_user(conn) do
+    conn.assigns.current_user
+  end
+
+  def first_name(conn) do
+    current_user = current_user(conn)
+    current_user.first_name
+  end
+end
